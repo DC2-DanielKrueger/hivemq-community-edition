b209ca52490a040826764d2992764b817a4159df
DC2-DanielKrueger <daniel.krueger@dc-square.de>
2021-04-16 15:47:12 +0200

    fix typo

--- src/packaging/bin/run.sh
+++ src/packaging/bin/run.sh
@@ -70,7 +70,7 @@ if hash java 2>/dev/null; then
     else
           echo "HIVEMQ_LOG_FOLDER is $HIVEMQ_LOG_FOLDER"
           echo "HIVEMQ_LOG_FOLDER is $HIVEMQ_LOG_FOLDER"
-          sudp mkdir -p $HIVEMQ_LOG_FOLDER
+          sudo mkdir -p $HIVEMQ_LOG_FOLDER
           sudo mkdir -p $HIVEMQ_DATA_FOLDER
           sudo chmod 777 $HIVEMQ_LOG_FOLDER
           sudo chmod 777 $HIVEMQ_DATA_FOLDER
